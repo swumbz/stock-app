e0a6ea21935d9771fe24697cc97c173b5dc66eb0
swumbz <mrkevinkeeler@gmail.com>
2024-07-23 18:42:16 -0700

    Attached FRED initial packages

--- Edgar Data.R
+++ Edgar Data.R
@@ -20,7 +20,7 @@ library(quantmod)
 library(edgar) # SEC data and filings from company
 library(tidyedgar) #clean up edgar package : https://github.com/gerardgimenezadsuar/tidyedgar 
 library(lubridate) # will parse datestamps into "quarters" using quarter fct
-library(fredr) # FRED data
+library(fredr) # FRED data 
 
 ###### DATA ANALYSIS #######
 
@@ -32,3 +32,24 @@ df_new <- subset(df, df$data.entityName=="APPLE INC.")
 revenue1 <- get_ydata(account = "Revenues")
 revenue_new <- subset(revenue1, revenue1$data.cik=="320193") # Apple CIK
 
+####### FRED Data for Interest Rate ########
+popular_funds_series <- fredr_series_search_text(
+  search_text = "federal funds",
+  order_by = "popularity",
+  sort_order = "desc",
+  limit = 1
+)
+
+popular_funds_series_id <- popular_funds_series$id
+
+popular_funds_series_id %>%
+  fredr(
+    observation_start = as.Date("1990-01-01"),
+    observation_end = as.Date("2024-07-01")
+  ) %>%
+ 
+   ggplot(data = ., mapping = aes(x = date, y = value, color = series_id)) +
+  geom_line() +
+  labs(x = "Observation Date", y = "Rate", color = "Series")
+
+####### FRED Data for ########
